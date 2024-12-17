1723b37d026bd90f09c1c37ae9d0f14d41898df6
mohamedelajour <mohamedelajour@gmail.com>
2024-12-17 11:31:13 +0200

    Add files via upload

---
+++ java/main_js.js
@@ -0,0 +1,525 @@
+
+ document.getElementById("by").onclick =  function () {
+    let to;
+    let zs=null;
+    document.getElementById("op").onclick =  function () 
+
+    { zs++
+        
+        if(zs%2==1)
+       {let bu= document.getElementById("mal")
+      bu.style.display="block"}
+    else
+    {let bu= document.getElementById("mal")
+        bu.style.display="none"}
+    }
+    let n= prompt("Enter the Number of Customer less than 8  ")
+    if(n>7)
+    {alert("The Number is Greater Than 8")}
+    else{
+    const arry1=[n]
+    let sum1=sum2=sum3=sum4=sum5=sum6=sum7=sum11= sum12=sum13=sum14=sum15=sum16=e1=e2=e3=e4=e5=e6=e7=e11=e12=e13=e14=e15=e16=
+    e17=t1=t2=t3=t4=t5=t6=t7=n1=n2=n3=n4=n5=n6=n7=y4=y1=y2=y3=y5=y6=y7=o1=o2=o3=o4=o5=o6=o7=z2=z3=z4=z5=z6=z7=v2=v3=v4=v5=v6=v7=sum17=null;
+
+for(let i=1;i<=n;i++){
+
+switch(i){
+  
+case 1:
+      let gr = document.getElementById("yt1")
+      gr.style.display="block"
+      gr.style.display="flex"
+      gr.style.justifycontent="space-between"
+      gr.style.alignitems="start"
+
+      document.getElementById("id_1").innerHTML =i
+
+    sum1= document.getElementById("id_ti1").innerHTML=Math.round(  Math.max(-1,0))
+
+    if(sum1>876)
+    {   sum11= document.getElementById("id_c1").innerHTML= 8 }
+else if(sum1>751)
+    {   sum11= document.getElementById("id_c1").innerHTML= 7 }
+else if(sum1>626)
+    {   sum11= document.getElementById("id_c1").innerHTML= 6 }
+else if(sum1>501)
+    {   sum11= document.getElementById("id_c1").innerHTML= 5}
+else if(sum1>376)
+    {   sum11= document.getElementById("id_c1").innerHTML= 4 }
+else if(sum1>251)
+    {   sum11= document.getElementById("id_c1").innerHTML= 3 }
+else if(sum1>126)
+    {   sum11= document.getElementById("id_c1").innerHTML= 2 }
+else 
+    {   sum11= document.getElementById("id_c1").innerHTML= 0}
+
+    document.getElementById("id_cl1").innerHTML=sum11
+e1=0
+    document.getElementById("id_p1").innerHTML=e1
+
+ e11=   document.getElementById("beg1").innerHTML=Math.round(  Math.random(1)*100)+1
+
+if(e11>95)
+    {n1= 6}
+else if(e11>85)
+    {n1= 5}
+else if(e11>60)
+    {n1= 4}
+else if(e11>30)
+    {n1= 3}
+else if(e11>10)
+    {n1= 2}
+else 
+    {n1= 1}
+    document.getElementById("id_t1").innerHTML= n1
+
+    t1=n1+e1
+    document.getElementById("end1").innerHTML=t1
+    document.getElementById("st1").innerHTML=0
+    document.getElementById("cs1").innerHTML=0
+    document.getElementById("cp1").innerHTML=t1
+
+
+
+
+            break;
+    
+case 2:
+    document.getElementById("id_2").innerHTML=i
+    sum2= document.getElementById("id_ti2").innerHTML=Math.round(  Math.random(1)*1000)+1
+
+    if(sum2>876)
+        {   sum12= document.getElementById("id_c2").innerHTML= 8 }
+else if(sum2>751)
+    {   sum12= document.getElementById("id_c2").innerHTML= 7 }
+else if(sum2>626)
+    {   sum12= document.getElementById("id_c2").innerHTML= 6 }
+else if(sum2>501)
+        {   sum12= document.getElementById("id_c2").innerHTML= 5}
+else if(sum2>376)
+            {   sum12= document.getElementById("id_c2").innerHTML= 4 }
+else if(sum2>251)
+    {   sum12= document.getElementById("id_c2").innerHTML= 3 }
+else if(sum2>126)
+        {   sum12= document.getElementById("id_c2").innerHTML= 2 }
+else 
+    {   sum12= document.getElementById("id_c2").innerHTML= 1}
+
+    e2=e1+sum12
+    document.getElementById("id_cl2").innerHTML=e2
+t2=Math.max(e2,t1)
+console.log(t2)
+    document.getElementById("id_p2").innerHTML=t2
+
+  e12=  document.getElementById("beg2").innerHTML=Math.round(  Math.random(1)*100)+1
+
+    if(e12>95)
+        {n2= 6}
+     else if(e12>85)
+         {n2= 5}
+     else if(e12>60)
+         {n2= 4}
+     else if(e12>30)
+         {n2= 3}
+     else if(e12>10)
+         {n2= 2}
+     else 
+         {n2= 1}
+
+         document.getElementById("id_t2").innerHTML= n2
+
+         y2=n2+t2
+         document.getElementById("end2").innerHTML=y2
+
+         z2=t2-e2
+         document.getElementById("st2").innerHTML=z2
+
+v2=t2-t1
+         document.getElementById("cs2").innerHTML=v2
+
+         document.getElementById("cp2").innerHTML=y2-e2
+
+    let gr2 = document.getElementById("yt2")
+    gr2.style.display="block"
+    gr2.style.display="flex"
+    gr2.style.justifycontent="space-between"
+    gr2.style.alignitems="start"
+let q=Number(e1)
+ o11=Number(sum11)
+ break;
+
+case 3:
+           document.getElementById("id_3").innerHTML=i
+
+        sum3= document.getElementById("id_ti3").innerHTML=Math.round(  Math.random(1)*1000)+1
+
+      if(sum3>876)
+        {   sum13= document.getElementById("id_c3").innerHTML= 8 }
+else if(sum3>751)
+    {   sum13= document.getElementById("id_c3").innerHTML= 7 }
+else if(sum3>626)
+    {   sum13= document.getElementById("id_c3").innerHTML= 6 }
+else if(sum3>501)
+        {   sum13= document.getElementById("id_c3").innerHTML= 5}
+else if(sum3>376)
+            {   sum13= document.getElementById("id_c3").innerHTML= 4 }
+else if(sum3>251)
+    {   sum13= document.getElementById("id_c3").innerHTML= 3 }
+else if(sum3>126)
+        {   sum13= document.getElementById("id_c3").innerHTML= 2 }
+else 
+    {   sum13= document.getElementById("id_c3").innerHTML= 1}
+
+    e3=e2+sum13
+    document.getElementById("id_cl3").innerHTML=e3
+    t3=Math.max(e3,y2)
+
+    document.getElementById("id_p3").innerHTML=t3
+
+    e13= document.getElementById("beg3").innerHTML=Math.round(  Math.random(1)*100)+1
+    if(e13>95)
+        {n3= 6}
+     else if(e13>85)
+         {n3= 5}
+     else if(e13>60)
+         {n3= 4}
+     else if(e13>30)
+         {n3= 3}
+     else if(e13>10)
+         {n3= 2}
+     else 
+         {n3= 1}
+     
+         document.getElementById("id_t3").innerHTML= n3
+         y3=n3+t3
+         document.getElementById("end3").innerHTML= y3
+
+         z3=t3-e3
+         document.getElementById("st3").innerHTML=z3
+
+         v3=t3-y2
+         document.getElementById("cs3").innerHTML=v3
+         document.getElementById("cp3").innerHTML=y3-e3
+
+    let gr3 = document.getElementById("yt3")
+    gr3.style.display="block"
+    gr3.style.display="flex"
+    gr3.style.justifycontent="space-between"
+    gr3.style.alignitems="start"
+        break;
+
+case 4:
+    document.getElementById("id_4").innerHTML=i
+
+    sum4= document.getElementById("id_ti4").innerHTML=Math.round(  Math.random(1)*1000)+1
+
+  if(sum4>876)
+    {   sum14= document.getElementById("id_c4").innerHTML= 8 }
+else if(sum4>751)
+{   sum14= document.getElementById("id_c4").innerHTML= 7 }
+else if(sum4>626)
+{   sum14= document.getElementById("id_c4").innerHTML= 6 }
+else if(sum4>501)
+    {   sum14= document.getElementById("id_c4").innerHTML= 5}
+else if(sum4>376)
+        {   sum14= document.getElementById("id_c4").innerHTML= 4 }
+else if(sum4>251)
+{   sum14= document.getElementById("id_c4").innerHTML= 3 }
+else if(sum4>126)
+    {   sum14= document.getElementById("id_c4").innerHTML= 2 }
+else 
+{   sum14= document.getElementById("id_c4").innerHTML= 1}
+
+e4=e3+sum14
+
+document.getElementById("id_cl4").innerHTML=e4
+t4=Math.max(e4,y3)
+
+document.getElementById("id_p4").innerHTML=t4
+
+e14= document.getElementById("beg4").innerHTML=Math.round(  Math.random(1)*100)+1
+if(e14>95)
+    {n4= 6}
+ else if(e14>85)
+     {n4= 5}
+ else if(e14>60)
+     {n4= 4}
+ else if(e14>30)
+     {n4= 3}
+ else if(e14>10)
+     {n4= 2}
+ else 
+     {n4= 1}
+ 
+     document.getElementById("id_t4").innerHTML= n4
+     y4=n4+t4
+     document.getElementById("end4").innerHTML= y4
+
+     z4=t4-e4
+     document.getElementById("st4").innerHTML=z4
+
+     v4=t4-y3
+     document.getElementById("cs4").innerHTML=v4
+     document.getElementById("cp4").innerHTML=y4-e4
+
+
+
+
+    let gr4 = document.getElementById("yt4")
+    gr4.style.display="block"
+    gr4.style.display="flex"
+    gr4.style.justifycontent="space-between"
+    gr4.style.alignitems="start"
+   
+   break;
+
+        case 5:
+            document.getElementById("id_5").innerHTML=i
+
+        sum5= document.getElementById("id_ti5").innerHTML=Math.round(  Math.random(1)*1000)+1
+
+        if(sum5>876)
+          {   sum15= document.getElementById("id_c5").innerHTML= 8 }
+      else if(sum5>751)
+      {   sum15= document.getElementById("id_c5").innerHTML= 7 }
+      else if(sum5>626)
+      {   sum15= document.getElementById("id_c5").innerHTML= 6 }
+      else if(sum5>501)
+          {   sum15= document.getElementById("id_c5").innerHTML= 5}
+      else if(sum5>376)
+              {   sum15= document.getElementById("id_c5").innerHTML= 4 }
+      else if(sum5>251)
+      {   sum15= document.getElementById("id_c5").innerHTML= 3 }
+      else if(sum5>126)
+          {   sum15= document.getElementById("id_c5").innerHTML= 2 }
+      else 
+      {   sum15= document.getElementById("id_c5").innerHTML= 1}
+      
+      e5=e4+sum15
+      
+      document.getElementById("id_cl5").innerHTML=e5
+      t5=Math.max(e5,y4)
+      
+      document.getElementById("id_p5").innerHTML=t5
+      
+      e15= document.getElementById("beg5").innerHTML=Math.round(  Math.random(1)*100)+1
+      if(e15>95)
+          {n5= 6}
+       else if(e15>85)
+           {n5= 5}
+       else if(e15>60)
+           {n5= 4}
+       else if(e15>30)
+           {n5= 3}
+       else if(e15>10)
+           {n5= 2}
+       else 
+           {n5= 1}
+       
+           document.getElementById("id_t5").innerHTML= n5
+           y5=n5+t5
+           document.getElementById("end5").innerHTML= y5
+      
+           z5=t5-e5
+           document.getElementById("st5").innerHTML=z5
+      
+           v5=t5-y4
+           document.getElementById("cs5").innerHTML=v5
+           document.getElementById("cp5").innerHTML=y5-e5
+      
+      
+      
+      
+          let gr5 = document.getElementById("yt5")
+          gr5.style.display="block"
+          gr5.style.display="flex"
+          gr5.style.justifycontent="space-between"
+          gr5.style.alignitems="start"
+         
+         break;
+   
+
+         case 6:
+            document.getElementById("id_6").innerHTML=i
+            
+            sum6= document.getElementById("id_ti6").innerHTML=Math.round(  Math.random(1)*1000)+1
+
+            if(sum6>876)
+              {   sum16= document.getElementById("id_c6").innerHTML= 8 }
+          else if(sum6>751)
+          {   sum16= document.getElementById("id_c6").innerHTML= 7 }
+          else if(sum6>626)
+          {   sum16= document.getElementById("id_c6").innerHTML= 6 }
+          else if(sum6>501)
+              {   sum16= document.getElementById("id_c6").innerHTML= 5}
+          else if(sum6>376)
+                  {   sum16= document.getElementById("id_c6").innerHTML= 4 }
+          else if(sum6>251)
+          {   sum16= document.getElementById("id_c6").innerHTML= 3 }
+          else if(sum6>126)
+              {   sum16= document.getElementById("id_c6").innerHTML= 2 }
+          else 
+          {   sum16= document.getElementById("id_c6").innerHTML= 1}
+          
+          e6=e5+sum16
+          
+          document.getElementById("id_cl6").innerHTML=e6
+          t6=Math.max(e6,y5)
+          
+          document.getElementById("id_p6").innerHTML=t6
+          
+          e16= document.getElementById("beg6").innerHTML=Math.round(  Math.random(1)*100)+1
+          if(e16>95)
+              {n6= 6}
+           else if(e16>85)
+               {n6= 5}
+           else if(e16>60)
+               {n6= 4}
+           else if(e16>30)
+               {n6= 3}
+           else if(e16>10)
+               {n6= 2}
+           else 
+               {n6= 1}
+           
+               document.getElementById("id_t6").innerHTML= n6
+               y6=n6+t6
+               document.getElementById("end6").innerHTML= y6
+          
+               z6=t6-e6
+               document.getElementById("st6").innerHTML=z6
+          
+               v6=t6-y5
+               document.getElementById("cs6").innerHTML=v6
+               document.getElementById("cp6").innerHTML=y6-e6
+             
+            let gr6 = document.getElementById("yt6")
+            gr6.style.display="block"
+            gr6.style.display="flex"
+            gr6.style.justifycontent="space-between"
+            gr6.style.alignitems="start"
+          
+        break;  
+
+
+
+case 7:
+    document.getElementById("id_7").innerHTML=i
+    
+    sum7= document.getElementById("id_ti7").innerHTML=Math.round(  Math.random(1)*1000)+1
+
+    if(sum7>876)
+      {   sum17= document.getElementById("id_c7").innerHTML= 8 }
+  else if(sum7>751)
+  {   sum17= document.getElementById("id_c7").innerHTML= 7 }
+  else if(sum7>626)
+  {   sum17= document.getElementById("id_c7").innerHTML= 6 }
+  else if(sum7>501)
+      {   sum17= document.getElementById("id_c7").innerHTML= 5}
+  else if(sum7>376)
+          {   sum17= document.getElementById("id_c7").innerHTML= 4 }
+  else if(sum7>251)
+  {   sum17= document.getElementById("id_c7").innerHTML= 3 }
+  else if(sum7>126)
+      {   sum17= document.getElementById("id_c7").innerHTML= 2 }
+  else 
+  {   sum17= document.getElementById("id_c7").innerHTML= 1}
+  
+  e7=e6+sum17
+  
+  document.getElementById("id_cl7").innerHTML=e7
+  t7=Math.max(e7,y6)
+  
+  document.getElementById("id_p7").innerHTML=t7
+  
+  e17= document.getElementById("beg7").innerHTML=Math.round(  Math.random(1)*100)+1
+  if(e17>95)
+      {n7= 6}
+   else if(e17>85)
+       {n7= 5}
+   else if(e17>60)
+       {n7= 4}
+   else if(e17>30)
+       {n7= 3}
+   else if(e17>10)
+       {n7= 2}
+   else 
+       {n7= 1}
+   
+       document.getElementById("id_t7").innerHTML= n7
+       y7=n7+t7
+       document.getElementById("end7").innerHTML= y7
+  
+       z7=t7-e7
+       document.getElementById("st7").innerHTML=z7
+  
+       v7=t7-y6
+       document.getElementById("cs7").innerHTML=v7
+       document.getElementById("cp7").innerHTML=y7-e7
+  
+
+
+    let gr7 = document.getElementById("yt7")
+    gr7.style.display="block"
+    gr7.style.display="flex"
+    gr7.style.justifycontent="space-between"
+    gr7.style.alignitems="start"
+ 
+
+
+
+      
+break;  
+ }}
+
+}
+
+
+
+
+
+
+
+
+// حدد العنصر canvas
+const ctx = document.getElementById('myChart').getContext('2d');
+
+// إنشاء الرسم البياني
+const myChart = new Chart(ctx, {
+    type: 'line', // نوع الرسم (bar, line, pie, etc.)
+    data: {
+        labels: ['Customer 1', 'Customer 2', 'Customer 3', 'Customer 4', 'Customer 5', 'Customer 6','Customer 7', 'Customer 8','Customer 9','Customer 10',],  
+        datasets: [{
+            label: ' (Interarrival Tiem)',
+            data: [sum11,sum12,sum13,sum14,sum15,sum16,sum17,], // البيانات
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true // يبدأ المحور y من الصفر
+            }
+        }
+    }
+});}
+
+
+
+
